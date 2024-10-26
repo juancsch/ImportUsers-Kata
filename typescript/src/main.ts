c93d80bfffd72dc477a1b23bc884d87f216dda4a
Juan Carlos Serrano <juancsch@gmail.com>
2024-10-26 17:14:20 +0200

    Define Golden Master test

--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -3,67 +3,72 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import fs from 'fs'
 
-/** This kata uses "fetch()", be aware you need at least Node 18 to run the script */
-const USER_URL = 'https://randomuser.me/api/?inc=gender,name,email,location&results=5&seed=a9b25cd955e2037h'
+export async function processUsers (log = console.log): Promise<void> {
 
-// Parse CSV file
-const filename = fileURLToPath(import.meta.url)
-const getcurrentworkingDirectory = path.dirname(filename)
+	/** This kata uses "fetch()", be aware you need at least Node 18 to run the script */
+	const USER_URL = 'https://randomuser.me/api/?inc=gender,name,email,location&results=5&seed=a9b25cd955e2037h'
 
-// fields: ID, gender, Name ,country, postcode, email, Birthdate
-const q = fs.readFileSync(
-	getcurrentworkingDirectory + '/../../users.csv', 'utf8'
-).split('\n')
+	// Parse CSV file
+	const filename = fileURLToPath(import.meta.url)
+	const getcurrentworkingDirectory = path.dirname(filename)
 
-const csvProvider: string[][] = []
-for (let h = 0; h < q.length; h++) {
-	if (q[h] === '') continue
-	csvProvider.push(q[h].split(','))
-}
+	// fields: ID, gender, Name ,country, postcode, email, Birthdate
+	const q = fs.readFileSync(
+		getcurrentworkingDirectory + '/../../users.csv', 'utf8'
+	).split('\n')
 
-const csvProviders: string[][] = []
-csvProviders.forEach((a) => {
-	a.concat(csvProvider[0])
-})
-csvProvider.shift() // Remove header column
-
-// Parse URL content
-const url = USER_URL
-
-const response = await (await fetch(url)).json() as { results: Array<{ gender: string, name: { first: string, last: string }, location: { country: string, postcode: string }, email: string }> }
-const webProvider = response.results // eslint-disable-line @typescript-eslint/prefer-destructuring
-
-const b: string[][] = []
-let i = 1_00_000_000_000
-for (let j = 0; j < webProvider.length; j++) {
-	i++
-	if (webProvider[j] instanceof Object) {
-		b.push([
-			i.toString(), // id
-			webProvider[j].gender,
-			webProvider[j].name.first + ' ' + webProvider[j].name.last,
-			webProvider[j].location.country,
-			webProvider[j].location.postcode,
-			webProvider[j].email,
-			new Date().getFullYear().toString() // birhtday
-		])
+	const csvProvider: string[][] = []
+	for (let h = 0; h < q.length; h++) {
+		if (q[h] === '') continue
+		csvProvider.push(q[h].split(','))
 	}
+
+	const csvProviders: string[][] = []
+	csvProviders.forEach((a) => {
+		a.concat(csvProvider[0])
+	})
+	csvProvider.shift() // Remove header column
+
+	// Parse URL content
+	const url = USER_URL
+
+	const response = await (await fetch(url)).json() as { results: Array<{ gender: string, name: { first: string, last: string }, location: { country: string, postcode: string }, email: string }> }
+	const webProvider = response.results // eslint-disable-line @typescript-eslint/prefer-destructuring
+
+	const b: string[][] = []
+	let i = 1_00_000_000_000
+	for (let j = 0; j < webProvider.length; j++) {
+		i++
+		if (webProvider[j] instanceof Object) {
+			b.push([
+				i.toString(), // id
+				webProvider[j].gender,
+				webProvider[j].name.first + ' ' + webProvider[j].name.last,
+				webProvider[j].location.country,
+				webProvider[j].location.postcode,
+				webProvider[j].email,
+				new Date().getFullYear().toString() // birhtday
+			])
+		}
+	}
+
+	/**
+	 * Shape: providers array[ id -> number,
+	 *                   email -> string
+	 *                   first_name -> string
+	 *                   last_name -> string ]
+	 */
+	const providers = csvProvider.concat(b) // merge arrays
+
+	// Print users
+	log('*********************************************************************************')
+	log('* ID\t\t* COUNTRY\t* NAME\t\t* EMAIL\t\t\t\t*')
+	log('*********************************************************************************')
+	for (let j = 0; j < providers.length; j++) {
+		log(`* ${providers[j][0]}\t* ${providers[j][3]}\t* ${providers[j][2]}\t* ${providers[j][5]}\t*`)
+	}
+	log('*********************************************************************************')
+	log(providers.length + ' users in total!')
 }
 
-/**
- * Shape: providers array[ id -> number,
- *                   email -> string
- *                   first_name -> string
- *                   last_name -> string ]
- */
-const providers = csvProvider.concat(b) // merge arrays
-
-// Print users
-console.log('*********************************************************************************')
-console.log('* ID\t\t* COUNTRY\t* NAME\t\t* EMAIL\t\t\t\t*')
-console.log('*********************************************************************************')
-for (let j = 0; j < providers.length; j++) {
-	console.log(`* ${providers[j][0]}\t* ${providers[j][3]}\t* ${providers[j][2]}\t* ${providers[j][5]}\t*`)
-}
-console.log('*********************************************************************************')
-console.log(providers.length + ' users in total!')
+processUsers().catch(console.error)
--- typescript/test/main.test.ts
+++ typescript/test/main.test.ts
@@ -1,10 +1,39 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
 import assert from 'node:assert'
 import { describe, test } from 'node:test'
 
+import { processUsers } from '../src/main.js'
+
 void describe('Import User Kata', () => {
 
-	void test('should ...', () => {
+	void test('should run process users', async () => {
+		// Given
+		const logs: string[] = []
+		const logSpy = (msg: string) => {
+			logs.push(msg)
+		}
 
-		assert.ok(true)
+		// When
+		await processUsers(logSpy)
+
+		// Then
+		assert.strictEqual(logs[0], '*********************************************************************************')
+		assert.strictEqual(logs[1], '* ID		* COUNTRY	* NAME		* EMAIL				*')
+		assert.strictEqual(logs[2], '*********************************************************************************')
+		assert.strictEqual(logs[3], '* 200189617246	* Germany	* Lukas Schmidt	* lukas.shmidt@example.com	*')
+		assert.strictEqual(logs[4], '* 200189016257	* Germany	* Maria Fischer	* maria.fischer@example.com	*')
+		assert.strictEqual(logs[5], '* 230573109005	* Spain 	* Luis Sanchez	* luis.sanchez@example.com	*')
+		assert.strictEqual(logs[6], '* 230854119034	* Italy 	* Elio Pausini	* elio.pausini@example.com	*')
+		assert.strictEqual(logs[7], '* 270054311737	* India 	* Mitesh Kumari	* mitesh.kumari@example.com	*')
+		assert.strictEqual(logs[8], '* 202160712259	* Germany	* Elena Mueller	* elena.muller@example.com	*')
+		assert.strictEqual(logs[9], '* 270554319031	* India 	* Natasha Shah	* natasha.shah@example.com	*')
+		assert.strictEqual(logs[10], '* 100000000001	* Australia	* Nevaeh Dunn	* nevaeh.dunn@example.com	*')
+		assert.strictEqual(logs[11], '* 100000000002	* Norway	* Sara Abdallah	* sara.abdallah@example.com	*')
+		assert.strictEqual(logs[12], '* 100000000003	* France	* Melvin Perrin	* melvin.perrin@example.com	*')
+		assert.strictEqual(logs[13], '* 100000000004	* Australia	* Dawn Snyder	* dawn.snyder@example.com	*')
+		assert.strictEqual(logs[14], '* 100000000005	* Netherlands	* Irina Kaptein	* irina.kaptein@example.com	*')
+		assert.strictEqual(logs[15], '*********************************************************************************')
+		assert.strictEqual(logs[16], '12 users in total!')
+
 	})
 })
